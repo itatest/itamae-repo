3d91878ba361440cc732c4360618284647f6e325
Takahiro Yano <yano_takahiro@ogis-ri.co.jp>
2015-12-03 18:24:05 +0900

    Use itamae-plugin-recipe-rbenv

---
+++ roles/app.rb
@@ -0,0 +1,17 @@
+node.reverse_merge!(
+  {
+    'rbenv' =>
+    {
+      'global' => '2.2.3',
+      'versions' => ['2.2.3'],
+      'rbenv_root' => '/usr/local/rbenv',
+      'scheme' => 'https'
+    },
+    'rbenv-default-gems' =>
+    {
+      'default-gems' => ['bundler']
+    }
+  }
+)
+
+include_recipe '../cookbooks/ruby/default.rb'
